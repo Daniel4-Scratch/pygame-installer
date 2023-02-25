87af40d9a4a50c17e83aa23ac3a1d87ca47bf650
Daniel Barton <daniel4scratchhelp@gmail.com>
2023-02-25 19:54:31 +1100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# Pygame Installer
+## What is this?
+Pygame Installer is an easy way to install, uninstall, update or reinstall pygame!
+## Requirements
+- Windows 7 or higher
+- Latest version of Python 3
+- Latest version of pip
+## How to download
+1. Download the zip file from [here]()
+2. Extract the zip file and save it to `c:\pygame` (or any other location you want)
+3. Done!
+## How to use
+1. Open command prompt
+2. Type `cd c:\pygame` and press enter (or the location you saved the zip file to)
+3. Type:
+    - `installer.bat install` to install pygame.
+    - `installer.bat uninstall` to uninstall pygame.
+    - `installer.bat update` to update pygame.
+    - `installer.bat reinstall` to reinstall pygame.
+    - `installer.bat help` to get help.
+4. Enjoy!
+___
+##### By Daniel4-Scratch
---
+++ installer.bat
@@ -0,0 +1,83 @@
+@echo off
+cls
+title Pygame Installer
+color 0a
+type resources\pygame
+echo pygame installer
+echo 0.0.1                 
+echo.
+
+set arg1=%1
+shift
+
+if "%arg1%"=="install" goto install
+if "%arg1%"=="uninstall" goto uninstall
+if "%arg1%"=="update" goto update
+if "%arg1%"=="reinstall" goto reinstall
+if "%arg1%"=="help" goto help
+goto error
+
+:install
+set /p TAB="Would you like to install pygame? (y/n)>"
+if "%TAB%"=="y" goto yesin
+if NOT "%TAB%"=="y" goto no
+exit
+
+:uninstall
+set /p TAB="Would you like to uninstall pygame? (y/n)>"
+if "%TAB%"=="y" goto yesun
+if NOT "%TAB%"=="y" goto no
+
+:update
+set /p TAB="Would you like to update pygame? (y/n)>"
+if "%TAB%"=="y" goto yesup
+if NOT "%TAB%"=="y" goto no
+
+:reinstall
+set /p TAB="Would you like to reinstall pygame? (y/n)>"
+if "%TAB%"=="y" goto yesre
+if NOT "%TAB%"=="y" goto no
+
+:help
+README.md
+exit
+
+:yesin
+echo Executing pip command...
+pip install pygame
+pause
+exit
+
+:yesun
+echo Executing pip command...
+pip uninstall pygame
+pause
+exit
+
+:yesup
+echo Executing pip command...
+pip install pygame --upgrade
+pause
+exit
+
+:yesre
+echo Executing pip command...
+pip install --force-reinstall --no-cache-dir pygame
+pause
+exit
+
+
+:no
+echo.
+echo Exiting...
+TIMEOUT /T 5
+exit
+
+:error
+echo no arguments given
+set /p TAB="Would you like to install or uninstall pygame? (in/un)>"
+if "%TAB%"=="in" goto install
+if "%TAB%"=="un" goto uninstall
+if NOT "%TAB%"=="in" goto no
+echo.
+pause
